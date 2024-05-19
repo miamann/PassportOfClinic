1f42ab08d35428cb0a1b8f63af04b1fbd7cd0b15
miamann <qwe635154@gmail.com>
2024-05-20 00:00:05 +0600

    добавление справочника, авторизации

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import main_view
+from .views import main_view, passport_view
+from . import views
 
 urlpatterns = [
     path('', main_view, name='main'),
+    path('passport/', passport_view, name='passport'), 
+    path('save_passport/', views.save_passport_mo, name='save_passport'),
+    path('reference_info/', views.reference_info, name='reference_info'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 def main_view(request):
     return render(request, 'main.html')
+
+def passport_view(request):
+    return render(request, 'passport.html')
+
+def save_passport_mo(request):
+    if request.method == 'POST':
+        return HttpResponse("Data saved successfully")
+    return redirect('passport')
+
+def reference_info(request):
+    return render(request, 'reference_info.html')
